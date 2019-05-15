16b9d972df8bf7f6cff16294ebb022d374e82fc8
Chris Barnes <barnesc@janelia.hhmi.org>
2019-05-15 17:56:39 -0400

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(here / "README.md") as f:
 
 setuptools.setup(
     name="fran",
-    version="0.6.1",
+    version="0.6.2",
     packages=["fran"],
     install_requires=[
         "imageio>=2.5.0",
