4da01de5e35927ba6ec4f2a31da52c101d019d09
Chris Barnes <barnesc@janelia.hhmi.org>
2019-06-06 13:46:21 -0400

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(here / "README.md") as f:
 
 setuptools.setup(
     name="fran",
-    version="0.8.1",
+    version="0.8.2",
     packages=["fran"],
     install_requires=[
         "imageio>=2.5.0",
