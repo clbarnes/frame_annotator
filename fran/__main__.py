e85c8e761660dfd36a2527c9ad62dd1299c6bbf6
Chris Barnes <barnesc@janelia.hhmi.org>
2019-12-10 13:51:43 +0000

    Fix config overriding

--- fran/__main__.py
+++ fran/__main__.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
 import logging
 import sys
-from argparse import ArgumentParser, RawTextHelpFormatter
+from argparse import ArgumentParser, RawTextHelpFormatter, Namespace
+from dataclasses import dataclass, field, replace
+from pathlib import Path
+from typing import Optional, Dict, Any
 
 import toml
 
@@ -9,21 +13,71 @@ from fran.common import parse_keys, setup_logging
 from fran.gui import run
 from fran.constants import (
     CONTROLS,
-    DEFAULT_FPS,
-    DEFAULT_CACHE_SIZE,
-    DEFAULT_THREADS,
-    DEFAULT_FLIPX,
-    DEFAULT_FLIPY,
-    DEFAULT_ROTATE,
-    DEFAULT_KEYS,
-    default_config,
+DEFAULT_FPS, DEFAULT_CACHE_SIZE, DEFAULT_THREADS,
+    default_config_dict,
 )
 
 
 logger = logging.getLogger(__name__)
 
 
-def parse_args():
+@dataclass
+class Config:
+    infile: Optional[Path] = None
+    write_config: Optional[Path] = None
+    outfile: Optional[Path] = None
+    config: Optional[Path] = None
+    fps: Optional[float] = None
+    cache: Optional[int] = None
+    threads: Optional[int] = None
+    keys: Dict[str, str] = field(default_factory=dict)
+    flipx: Optional[bool] = None
+    flipy: Optional[bool] = None
+    rotate: Optional[float] = None
+    version: Optional[bool] = None
+    verbose: Optional[int] = None
+    logfile: Optional[Path] = None
+
+    def replace_from_namespace(self, ns: Namespace) -> Config:
+        return self.replace(**{k: v for k, v in vars(ns).items() if v is not None})
+
+    def replace(self, keys=None, **kwargs) -> Config:
+        """N.B. 'keys' is an update, not a replacement"""
+        if keys:
+            union = self.keys.copy()
+            union.update(keys)
+        else:
+            union = self.keys
+
+        return replace(self, keys=union, **kwargs)
+
+    def replace_from_config_dict(self, d: Dict[str, Dict[str, Any]]) -> Config:
+        flat = d.get("settings", dict()).copy()
+        flat.update(d.get("transform", dict()).copy())
+        flat["keys"] = d.get("keys", dict()).copy()
+        return self.replace(**flat)
+
+    def replace_from_toml(self, fpath) -> Config:
+        return self.replace_from_config_dict(toml.load(fpath))
+
+    def write_toml(self, fpath):
+        d = {
+            "settings": {
+                "fps": self.fps,
+                "cache": self.cache,
+                "threads": self.threads,
+            },
+            "transform": {
+                "flipx": self.flipx,
+                "flipy": self.flipy,
+                "rotate": self.rotate,
+            },
+            "keys": self.keys,
+        }
+        toml.dump(d, fpath)
+
+
+def parse_args(args=None) -> Namespace:
     parser = ArgumentParser(
         description="Log video (multipage TIFF) frames in which an event starts or ends",
         epilog=CONTROLS,
@@ -33,34 +87,36 @@ def parse_args():
     parser.add_argument(
         "--write_config",
         "-w",
+        type=Path,
         help="Write back the complete config to a file at this path, then exit",
     )
     parser.add_argument(
         "--outfile",
         "-o",
+        type=Path,
         help="Path to CSV for loading/saving. "
-        "If no path is selected when you save, a file dialog will open.",
+             "If no path is selected when you save, a file dialog will open.",
     )
     parser.add_argument("--config", "-c", help="Path to TOML file for config")
     parser.add_argument(
         "--fps",
         "-f",
         type=float,
-        default=DEFAULT_FPS,
+        default=None,
         help=f"Maximum frames per second; default {DEFAULT_FPS}",
     )
     parser.add_argument(
         "--cache",
         "-n",
         type=int,
-        default=DEFAULT_CACHE_SIZE,
+        default=None,
         help=f"Number of frames to cache (increase if reading over a network and you have lots of RAM); default {DEFAULT_CACHE_SIZE}",
     )
     parser.add_argument(
         "--threads",
         "-t",
         type=int,
-        default=DEFAULT_THREADS,
+        default=None,
         help=f"number of threads to use for reading file (increase if reading over a network); default {DEFAULT_THREADS}",
     )
     parser.add_argument(
@@ -69,27 +125,27 @@ def parse_args():
         type=parse_keys,
         default=dict(),
         help='Optional mappings from event name to key, in the format "w=forward,a=left,s=back,d=right". '
-        "These are additive with those defined in the config",
+             "These are additive with those defined in the config",
     )
     parser.add_argument(
         "--flipx",
         "-x",
         action="store_true",
-        default=DEFAULT_FLIPX,
+        default=None,
         help="Flip image in x",
     )
     parser.add_argument(
         "--flipy",
         "-y",
         action="store_true",
-        default=DEFAULT_FLIPY,
+        default=None,
         help="Flip image in y",
     )
     parser.add_argument(
         "--rotate",
         "-r",
         type=float,
-        default=DEFAULT_ROTATE,
+        default=None,
         help="Rotate image (degrees counterclockwise; applied after flipping)",
     )
     parser.add_argument(
@@ -105,17 +161,24 @@ def parse_args():
     parser.add_argument(
         "infile",
         nargs="?",
+        type=Path,
         default=None,
         help="Path to multipage TIFF file to read. If no path is given, a file dialog will open.",
     )
 
-    parsed = parser.parse_args()
+    return parser.parse_args(args)
 
-    setup_logging(parsed.verbose, parsed.logfile)
 
-    if parsed.version:
-        print(__version__)
-        sys.exit(0)
+def generate_config():
+    config = Config().replace_from_config_dict(default_config_dict)
+
+    parsed = parse_args()
+    if parsed.config:
+        logger.info("Loading config file from %s", config.write_config)
+        config = config.replace_from_toml(parsed.config)
+    return config.replace_from_namespace(parsed)
+
+    return config
 
     keys_mapping = DEFAULT_KEYS.copy()
 
@@ -129,13 +192,13 @@ def parse_args():
                     parsed,
                     setting_key,
                     config.get("settings", dict()).get(
-                        setting_key, default_config["settings"][setting_key]
+                        setting_key, default_config_dict["settings"][setting_key]
                     ),
                 )
 
-        keys_mapping.update(config.get("keys", dict()))
+        keys_mapping.replace(config.get("keys", dict()))
 
-    keys_mapping.update(parsed.keys)
+    keys_mapping.replace(parsed.keys)
     parsed.keys = keys_mapping
 
     if parsed.write_config:
@@ -161,17 +224,29 @@ def parse_args():
 
 
 def main():
-    parsed_args = parse_args()
+    config = generate_config()
+
+    setup_logging(config.verbose, config.logfile)
+
+    if config.version:
+        print(__version__)
+        sys.exit(0)
+
+    if config.write_config:
+        logger.info("Writing config file to %s and exiting", config.write_config)
+        config.write_toml(config.write_config)
+        sys.exit(0)
+
     return run(
-        parsed_args.infile,
-        parsed_args.outfile,
-        parsed_args.cache,
-        parsed_args.fps,
-        parsed_args.threads,
-        parsed_args.keys,
-        parsed_args.flipx,
-        parsed_args.flipy,
-        parsed_args.rotate,
+        config.infile,
+        config.outfile,
+        config.cache,
+        config.fps,
+        config.threads,
+        config.keys,
+        config.flipx,
+        config.flipy,
+        config.rotate,
     )
 
 
--- fran/constants.py
+++ fran/constants.py
@@ -6,15 +6,15 @@ here = Path(__file__).absolute()
 package_dir = here.parent
 config_path = package_dir / "config.toml"
 
-default_config = toml.load(config_path)
+default_config_dict = toml.load(config_path)
 
-DEFAULT_CACHE_SIZE = default_config["settings"]["cache"]
-DEFAULT_FPS = default_config["settings"]["fps"]
-DEFAULT_THREADS = default_config["settings"]["threads"]
-DEFAULT_FLIPX = default_config["transform"]["flipx"]
-DEFAULT_FLIPY = default_config["transform"]["flipy"]
-DEFAULT_ROTATE = default_config["transform"]["rotate"]
-DEFAULT_KEYS = default_config["keys"]
+DEFAULT_CACHE_SIZE = default_config_dict["settings"]["cache"]
+DEFAULT_FPS = default_config_dict["settings"]["fps"]
+DEFAULT_THREADS = default_config_dict["settings"]["threads"]
+DEFAULT_FLIPX = default_config_dict["transform"]["flipx"]
+DEFAULT_FLIPY = default_config_dict["transform"]["flipy"]
+DEFAULT_ROTATE = default_config_dict["transform"]["rotate"]
+DEFAULT_KEYS = default_config_dict["keys"]
 
 controls_path: Path = package_dir / "controls.txt"
 
